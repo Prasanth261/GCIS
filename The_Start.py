8bb3c46e1cc934465fd33eefe0b3388934e68ba8
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-05 10:00:18 +0400

    First commit/added happy birthday.py

---
+++ The_Start.py
@@ -0,0 +1,14 @@
+# import pyfiglet
+word=input("Enter text to print: ")
+# f = pyfiglet.figlet_format(word, font="standard")
+# print(f)
+
+# print(input("Enter a second text to print: "))
+
+# _prasanth=123
+# print(_prasanth)
+
+
+print(f'HEY! {word}')
+
+# print("We are in \"GCIS123\" course, in \"RIT Dubai\", in morning session.")
