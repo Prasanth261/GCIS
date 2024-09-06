739cd34043c47fa19970782efe42192c089a0af7
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-06 10:39:00 +0400

    added variables.py

---
+++ variables.py
@@ -0,0 +1,34 @@
+def variable_practice():
+    age = int(input("Enter age: "))
+    age_m= age*12
+    year= 365
+    name_pet=str(input("Enter ur pet name: "))
+    Pi_5_digits=float(input("Enter 5 first digits of pi: "))
+    print("age:",age,"age in months:",age_m,"year:",year,"name of the pet:",name_pet,"pi 5 digits:",Pi_5_digits)
+    # print(f"Age in months: {age_m},Number of days in a year: {year},name of ur first pet: {name_pet},first 5 digits of pi: {Pi_5_digits}")
+
+
+
+
+
+
+
+
+
+def expressions_practice():
+    Literal="Prasanth"
+    Addition=2+2
+    Subraction=2-2
+    Exponent=2**2
+    Floor_Division=2//2
+    Mod=2%2
+    parenthesis=(Addition + Subraction)*26
+    Mix=(7*8)-(10%5)//2
+    print("Literal:",Literal,"Addtion:",Addition,"Subraction:",Subraction,"Exponent:",Exponent,"Floor_Division:",Floor_Division,"Mod:",Mod,"parenthesis:",parenthesis,"Mix:",Mix)
+     #print(f"Literal:{Literal}\nAddtion:{Addition}\nSubraction:{Subraction}\nExponent:{Exponent}\nFloor_Division:{Floor_Division}\nMod:{Mod}\nparenthesis:{parenthesis}\nMix:{Mix}\n")
+# def main():
+#     variable_practice()
+#     expressions_practice()
+# main()
+# variable_practice()
+expressions_practice()
