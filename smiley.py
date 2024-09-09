25a5db86ab6c562204b084d99bc22e5ff7a87387
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-09 21:34:54 +0400

    added smiley.py and did few activities

--- smiley.py
+++ smiley.py
@@ -1,28 +1,50 @@
 import turtle
 
-def draw_circle(color='Red',x=0,y=0,rad=10):
-    t=turtle.Turtle()
+t=turtle
+
+def draw_circle(color='Red',rad=10):
     t.fillcolor(color)
     t.up()
-    t.goto(x,y)
+    t.forward(rad)
+    t.left(90)
     t.down()
     t.begin_fill()
     t.circle(rad)
     t.end_fill()
+    t.up()
+    t.left(90)
+    t.forward(rad)
+    t.right(180)
     print(t.xcor())
     print(t.ycor())
+
+def draw_smiley(color,radius):
+    draw_circle('yellow',radius)
+    draw_circle('LightCoral',radius/12)
+    # draw_eye(radius*0.35,radius*0.25,color,radius*0.25)
+    # draw_eye(-(radius*0.35),radius*0.25,color,radius*0.25)
+    draw_eye(radius/3,radius/3,color,radius/4)
+    draw_eye(-radius/3,radius/3,color,radius/4)
+
     input("Enter to continue: ")
 
-def draw_smiley():
-    radius=150
-    draw_circle('yellow',0,0,radius)
-    draw_circle('LightCoral',0,radius-10,10)
+def draw_eye(x,y,color,radius):
+    t.up()
+    t.goto(x,y)
+    t.down()
+    draw_circle('white',radius)
+    draw_circle(color,radius/2)
+    draw_circle('black',radius/4)
+
+
+def tweak():
+    t.speed(5)
+    t.tracer(False)
+    t.hideturtle()
 
 def main():
-    # radius=150
-    # draw_circle('red',0,0,radius)
-    # draw_circle('red',0,radius-10,10)
-    draw_smiley()
-main()
+    tweak()
+    draw_smiley('blue',100)
 
+main()
 
