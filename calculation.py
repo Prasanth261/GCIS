6cfe85fbc227a35af572a530c9bba35a3f2195a1
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-19 09:30:39 +0400

    made a new folder for smiley to reduce space and added calculation and testing

---
+++ calculation.py
@@ -0,0 +1,14 @@
+"""Addition,multiplication,subraction 
+and division with 2 parameters"""
+
+
+
+
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiple(a,b):
+    return a*b
+def division(a,b):
+    return a/b
---
+++ testing.py
@@ -0,0 +1,13 @@
+def cube(x):
+    cube=x**3
+    return cube
+def multiply(x,y):
+    return x+y
+def test_cube():
+    assert cube(2)==8
+def test_multiply():
+    assert multiply(2,3) == 6
+def main():
+    test_cube()
+    test_multiply()
+main()
