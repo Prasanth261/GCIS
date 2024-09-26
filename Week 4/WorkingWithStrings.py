2422519cc101361bfa11b60c7b81c222f68d34ea
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-26 09:56:46 +0400

    added workingwithstrings.py

---
+++ Week 4/WorkingWithStrings.py
@@ -0,0 +1,24 @@
+print("Today is a \"Sunny\" Day")
+print("Today is a\tSunny Day")
+print("Today is a\nSunny Day")
+print("Today is a\bSunny Day")
+print("C:\\Users\\Sunny Day\\")
+
+
+print("She said \"I don't like broccoli.\"")
+print("A\tB\tC\tD\tE")
+print("This/is\\a/test\\.")
+
+
+print("she\tsaid\"I \\ \n")
+print("don't\tlike \\ \n")
+print("broccoli.\"\n")
+
+string="Prasanth"
+count=0
+
+while count<len(string):
+    print(string[count])
+    count+=1
+    
+
