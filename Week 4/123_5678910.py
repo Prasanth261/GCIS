5688ffd58eca696ae9df3c5f7ea0948f51474844
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-09-26 11:43:45 +0400

    Added a lot of programs

---
+++ Week 4/123_5678910.py
@@ -0,0 +1,7 @@
+count=0
+while count<=10:
+    if count==4:
+        count+=1
+        continue
+    print(count)
+    count+=1
---
+++ Week 4/range_loop.py
@@ -0,0 +1,7 @@
+def print_range(range):
+    count=range[0]
+    while count<=range[-1]:
+        print(count,end=" ")
+        count+=1
+r1_5=range(4,10)
+print_range(r1_5)
