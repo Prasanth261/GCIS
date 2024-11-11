6ab4ff2422df6599c2ee0c0d9893e692562985cf
Prasanth Sathiya <s.prasanth2611@gmail.com>
2024-11-11 22:11:13 +0400

    did some work on group and merge_sort file

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,39 +1,38 @@
-arr=[1,123,53,13,643,13,5,1,3]
+arr=[1,123,53,13,643,13,5,3]
 
 def merge_sort(arr):
     if len(arr) > 1:
-        ratio = 0.5  # 70-30 split
+
+        ratio = 0.5  #50-50 split
         split_index = int(len(arr) * ratio)
         left_arr = arr[:split_index]
         right_arr = arr[split_index:]
+        
         merge_sort(left_arr)
         merge_sort(right_arr)
         
-        i = 0  # left array index
-        j = 0  # right array index
-        k = 0  # merged array index
+
+        i=0
+        j=0
+
         
-        # Compare and merge elements
-        while i < len(left_arr) and j < len(right_arr):
-            if left_arr[i] < right_arr[j]:
+        for k in range(len(arr)):
+            if i < len(left_arr) and j < len(right_arr):
+                if left_arr[i] < right_arr[j]:
+                    arr[k] = left_arr[i]
+                    i += 1
+                else:
+                    arr[k] = right_arr[j]
+                    j += 1
+
+            
+            elif i < len(left_arr):
                 arr[k] = left_arr[i]
                 i += 1
+
             else:
                 arr[k] = right_arr[j]
                 j += 1
-            k += 1
-        
-        # Add remaining elements from left
-        while i < len(left_arr):
-            arr[k] = left_arr[i]
-            i += 1
-            k += 1
-        
-        # Add remaining elements from right    
-        while j < len(right_arr):
-            arr[k] = right_arr[j]
-            j += 1
-            k += 1
             
     return arr
 
